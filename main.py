3d399c182aa2fba67db394bd7d5d440955ae7326
Xwar5 <130825226+Xwar5@users.noreply.github.com>
2024-02-09 18:58:25 +0200

    basic server strucrture

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, redirect
+import settings as stg
+
+app = Flask (__name__, static_folder=stg.PATH_STATIC )
+app.config["SECRET_KEY"] = stg.SECRET_KEY
+
+@app.route("/")
+def index():
+    url = url_for("static", filename="templates/index.html")
+    return redirect(url)
+
+
+
+app.run(debug=True)
---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+PATH = os.path.dirname(__file__) + os.sep
+PATH_STATIC = PATH + "static" + os.sep
+
+SECRET_KEY = "F268CA565EC92288E26DCF99B3855"
