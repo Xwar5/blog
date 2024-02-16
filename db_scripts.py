24b0cd52e0f185b74b2c9bb8a31d7ebe8b1a6176
Artem <artic21062008@gmail.com>
2024-02-16 20:14:25 +0200

    basic server strucrture

---
+++ db_scripts.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+db_name = "blog.db"
+connection = None
+cursor = None
+
+def open():
+    global connection, cursor
+    connection = sqlite3.connect(db_name)
+    cursor = connection.cursor()
+
+def close():
+    cursor.close()
+    connection.close()
