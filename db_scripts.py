d1d20d8fb0566e37421c67f80d7fb39b4b99c667
Xwar5 <za2ha2r8@gmail.com>
2024-02-16 20:25:13 +0200

    lesson 16.02

--- db_scripts.py
+++ db_scripts.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3
 
 db_name = "blog.db"
@@ -12,19 +11,4 @@ def open():
 
 def close():
     cursor.close()
-=======
-import sqlite3
-
-db_name = "blog.db"
-connection = None
-cursor = None
-
-def open():
-    global connection, cursor
-    connection = sqlite3.connect(db_name)
-    cursor = connection.cursor()
-
-def close():
-    cursor.close()
->>>>>>> 3d399c182aa2fba67db394bd7d5d440955ae7326
     connection.close()
--- settings.py
+++ settings.py
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import os
 
 PATH = os.path.dirname(__file__) + os.sep
 PATH_STATIC = PATH + "static" + os.sep
 
-=======
-import os
-
-PATH = os.path.dirname(__file__) + os.sep
-PATH_STATIC = PATH + "static" + os.sep
-
->>>>>>> 3d399c182aa2fba67db394bd7d5d440955ae7326
 SECRET_KEY = "F268CA565EC92288E26DCF99B3855"
